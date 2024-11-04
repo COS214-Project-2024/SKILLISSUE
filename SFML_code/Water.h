a2816b2d9882afafeb7169ca88260a288324056d
HenruMatthis <u23526158@tuks.co.za>
2024-11-04 16:56:34 +0200

    Added Doxygen comments to WasteManagement.h WasteManagementFactory.h Water.h WaterFactory.h WaterPlant.h WaterPlantFactory.h

--- SFML_code/Water.h
+++ SFML_code/Water.h
@@ -5,11 +5,26 @@
 #include <vector>
 #include "Tile.h"
 
+/**
+ * @class Water
+ * @brief Represents a water tile in the simulation.
+ * 
+ * The Water class inherits from Tile and defines properties and behaviors specific to water tiles,
+ * which may be used to represent natural water bodies or resources within the simulation.
+ */
 class Water : public Tile
 {
 public:
 
-    /* Constructor */
+    /**
+     * @brief Constructs a Water tile with a given texture and animations.
+     * 
+     * Initializes the Water tile with the specified texture and animation sequence,
+     * setting it up for rendering and interaction within the simulation.
+     * 
+     * @param texture Reference to the texture applied to the water tile.
+     * @param animations Vector containing animations associated with this tile.
+     */
     Water(sf::Texture &texture, const std::vector<Animation> &animations);
 
 };
