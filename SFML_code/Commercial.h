82de1f4ded76a89823f3c4c2b2476bac6c8dae6b
HenruMatthis <u23526158@tuks.co.za>
2024-11-04 15:19:33 +0200

    Added Doxygen comments to CityMediator.h Command.h, Commercial.h, CommercialFactory.h

--- SFML_code/Commercial.h
+++ SFML_code/Commercial.h
@@ -5,13 +5,25 @@
 #include <vector>
 #include "Tile.h"
 
+/**
+ * @class Commercial
+ * @brief Represents a commercial tile in the simulation.
+ * 
+ * The Commercial class inherits from Tile and is used to define properties and
+ * behaviors specific to commercial zones within the simulation.
+ */
 class Commercial : public Tile
 {
 public:
 
-    /* Constructor */
+    /**
+     * @brief Constructs a Commercial tile with a given texture and animations.
+     * 
+     * @param texture Reference to the texture applied to the commercial tile.
+     * @param animations Vector containing animations associated with this tile.
+     */
     Commercial(sf::Texture &texture, const std::vector<Animation> &animations);
 
 };
 
-#endif /* TILE_HPP */
+#endif /* COMMERCIAL_H */
--- SFML_code/CommercialFactory.h
+++ SFML_code/CommercialFactory.h
@@ -5,11 +5,39 @@
 #include "TileFactory.h"
 #include "Commercial.h"
 
+/**
+ * @class CommercialFactory
+ * @brief Factory class for creating Commercial tiles.
+ * 
+ * The CommercialFactory class is responsible for creating instances of 
+ * Commercial tiles, following the factory pattern to encapsulate the instantiation process.
+ */
 class CommercialFactory : public TileFactory
 {
 protected:
+    /**
+     * @brief Creates a new Commercial tile with the specified texture and animations.
+     * 
+     * This protected method is overridden to provide the implementation for creating
+     * a Commercial tile with the given texture and animations.
+     * 
+     * @param texture Reference to the texture to apply to the tile.
+     * @param animations Vector containing animations for the tile.
+     * @return A pointer to the created Commercial tile.
+     */
     virtual Tile* CreateTile(sf::Texture &texture, const std::vector<Animation> &animations);
 public:
+
+    /**
+     * @brief Creates and returns a new Commercial tile.
+     * 
+     * This method is publicly accessible and uses the factory pattern to create
+     * a new Commercial tile with the provided texture and animations.
+     * 
+     * @param texture Reference to the texture for the tile.
+     * @param animations Vector of animations associated with the tile.
+     * @return A pointer to the newly created Commercial tile.
+     */ 
     Tile* Create(sf::Texture &texture, const std::vector<Animation> &animations);
 
     
